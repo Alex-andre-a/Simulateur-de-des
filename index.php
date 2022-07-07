1e49ca3406787bd435934c6ac4ee2289d0a30cbb
@l€x <ptitmitch44@gmail.com>
2022-07-07 16:34:31 +0200

    En cours !

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>simulateur de dés</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="titre">
+            <h1>Le Sloubi</h1>
+        </div>
+        <div class="menus">
+            <a href="">Dé à 6 faces</a>
+            <a href="">Dé à 8 faces</a>
+            <a href="">Dé à 10 faces</a>
+            <a href="">Dé à 12 faces</a>
+        </div>
+    </header>
+    <main>
+
+        <form>
+        <input type="button" value="A vous de jouer !" >
+
+
+
+        <p>
+            <img src="/assets/6-faces/<?php echo rand(1, 6); ?>.png" alt="">
+        </p>
+        
+        </form>
+
+        
+
+    </main>
+
+    
+</body>
+</html>
